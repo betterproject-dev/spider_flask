b72843973fb8af2a819bb3e0c736fbb4139e1d4b
sysong0516 <sysong777@naver.com>
2026-01-05 17:16:40 +0900

    중간test

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from .config import config
 from .extensions import db, migrate, cors, socketio
-from .mqtt import init_mqtt
+# from .mqtt import init_mqtt
 
 def create_app():
   app = Flask(__name__)
@@ -21,7 +21,7 @@ def create_app():
   
   from .blueprints.sensormodel import bp as sensormodel_bp
 
-  app.mqtt_client = init_mqtt(app)
+  # app.mqtt_client = init_mqtt(app)
   
   # === blueprints ===
   from .blueprints.test import bp as test_bp # 테스트용(삭제)
